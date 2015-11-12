db804f7de4068a63c07a503331063dfa3cd1c903
Hyun Jin <freejak5520@naver.com>
2015-11-12 23:47:51 +0900

    초기 세팅 건드는중

--- HighTempler/src/main/java/ht/controller/ContentController.java
+++ HighTempler/src/main/java/ht/controller/ContentController.java
@@ -1,9 +1,27 @@
 package ht.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ht.mongo.MongoDB;
+
+@Controller
 public class ContentController {
 
 	public ContentController() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	@RequestMapping("test.do")
+	public ModelAndView test() {
+		
+		MongoDB md = new MongoDB();
+		
+		String result = (String)md.test();
+		
+		ModelAndView mav = new ModelAndView("jsonView");
+		mav.addObject("result", result);
+		return mav;
+	}
 }
