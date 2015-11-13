b331f294f3977d8f6426dfb379b82824b986993d
Hyun Jin Lee <freejak5520@naver.com>
2015-11-14 00:30:20 +0900

    HomeController에 테스트 맵핑 추가
    MongoDB쪽 필요없는 부분 삭제

--- HighTempler/src/main/java/ht/controller/HomeController.java
+++ HighTempler/src/main/java/ht/controller/HomeController.java
@@ -2,6 +2,9 @@ package ht.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ht.mongo.MongoDB;
 
 @Controller
 public class HomeController {
@@ -13,5 +16,15 @@ public class HomeController {
 	@RequestMapping("/home.do")
 	public String home() {
 		return "home";
+	}
+	
+	@RequestMapping("/test.do")
+	public ModelAndView test() {
+		
+		MongoDB md = new MongoDB();
+		md.test();
+		
+		ModelAndView mav = new ModelAndView("jsonView");
+		return mav;
 	}
 }
--- HighTempler/src/main/java/ht/mongo/MongoDB.java
+++ HighTempler/src/main/java/ht/mongo/MongoDB.java
@@ -1,9 +1,45 @@
 package ht.mongo;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
 public class MongoDB {
 
 	public MongoDB() {
-		// TODO Auto-generated constructor stub
+		
+		
+		
+		
 	}
 
+	public void test() {
+		
+		MongoClientURI uri = new MongoClientURI("mongodb://user22:user22@ds053184.mongolab.com:53184/lol");
+		MongoClient client = new MongoClient(uri);
+		
+		MongoDatabase db = client.getDatabase("lol");
+		
+		MongoCollection<Document> coll = db.getCollection("temple");
+		
+		Document op = new Document("name", "이현");
+		
+		FindIterable<Document> resultList = coll.find(op);
+		
+		
+		
+		MongoCursor<Document> cursor = resultList.iterator();
+		
+		while(cursor.hasNext()) {
+			
+			System.out.println(cursor.next());
+			
+		}
+	}
+	
 }
